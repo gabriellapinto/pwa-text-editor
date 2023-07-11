6b5bf295888e2ab60a8dbc75d18932677f83f9a8
Gabby <gabbympinto@gmail.com>
2023-07-11 12:50:52 -0400

    fixed webpack config, and js logic

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pwa-text-editor
+
+## About this Application
+This app is a text editor that meets the PWA (Progressive Web Application) criteria. It uses a package called "idb", which is a lightweight wrapper around the IndexedDB API. This app also features a number of methods that are useful for storing and retrieving data, and is used by companies like Google and Mozilla.
+
+## Link to Live Application
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,32 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    await store.add({ content: content });
+    await tx.complete;
+    console.log('putDb complete', content);
+  } catch (error) {
+    console.error('putDb not implemented');
+  }
+};
+
+
+export const getDb = async () => {
+  try {
+    const db = openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const content = await store.getAll();
+    console.log('getDb complete', content);
+    return content;
+  } catch (error) {
+    console.error('getDb not implemented'), error;
+  }
+};
 
 initdb();
